69a48a7f0cb19e7f82951896911b5406ac4a51d3
Christopher Whitney <chrisswhitneyy@gmail.com>
2016-11-16 17:58:39 -0700

    defined semantic function up to TRUE and FALSE. Working on EQ next.

--- Project2/project2.v
+++ Project2/project2.v
@@ -27,42 +27,45 @@ Definition config : Type := ( code * Stack.t * State.t) .
 Inductive am : config -> config -> Prop := 
 | am_noop:
     forall (c:code) (e:Stack.t)(s:State.t) ,
-      am  (c, e, s)  (c , e, s).
-(*
+      am  (c, e, s)  (c , e, s)
 | am_push: 
-  forall  (n:Z) (s:State.t Type) (e:Stack.t), 
-      am (PUSH n)  s  (Fin (Stack.push n e)).
+  forall  (n:Z) (c:code) (e:Stack.t)(s:State.t), 
+      am ((PUSH n)::c, e, s) (c, (Stack.z n)::e, s)
 
+(* Z operations *)
 | am_add: 
-  forall (z1 z2: Z) (s:State.t)  (e:Stack.t), 
-     (Stack.push (ZArith.add Stack.pop Stack.pop))
+  forall (z1 z2: Z) (c:code) (s:State.t)  (e:Stack.t), 
+     am (ADD::c, e, s) (c, (Stack.z (Z.add z1 z2))::e, s)
 
 | am_sub: 
-  forall (z1 z2: Z) (s:State.t)  (e:Stack.t), 
-     (Stack.push (ZArith.sub Stack.pop Stack.pop))
+  forall (z1 z2: Z) (c:code) (s:State.t)  (e:Stack.t), 
+     am (SUB::c, e, s) (c, (Stack.z (Z.sub z1 z2))::e, s)
 
 | am_mult: 
-  forall (z1 z2: Z) (s:State.t)  (e:Stack.t), 
-     (Stack.push (ZArith.mult Stack.pop Stack.pop))
+  forall (z1 z2: Z) (c:code) (s:State.t)  (e:Stack.t), 
+     am (MULT::c, e, s) (c, (Stack.z (Z.mul z1 z2))::e, s)
+
+(* TRUE and FALSE *)
+| am_true: 
+    forall (c:code) (s:State.t) (e:Stack.t) (tt:bool), 
+       am (TRUE::c, e, s) (c, (Stack.T tt)::e, s)
+
+| am_false: 
+    forall (c:code) (s:State.t) (e:Stack.t) (ff:bool), 
+       am (FALSE::c, e, s) (c, (Stack.T ff)::e, s).
+
+(*
+
+(* EQ and LE *) 
+| am_eq_tt : 
+  forall (c:code) (s:State.t) (e:Stack.t) (z1 z2: Z), 
+    am (EQ::c, e, s) (c, (Stack.T (is_eq z1 z2))::e, s).
 
 | am_fetch: 
     forall (n:Id.t)  (s:State.t Type) (e:Stack.t), (State.find s n) ::e. 
 
-| am_true: 
-    forall (tt:bool) (s:State.t)  (e:Stack.t), 
-    Stack.push tt e. 
 
-|am_false: 
-   forall (ff:bool) (s:State.t)  (e:Stack.t), 
-    Stack.push ff e.
 
-| am_eq: 
-  forall (z1 z2: Z) (s:State.t)  (e:Stack.t), 
-     (Stack.push (ZArith.eq Stack.pop Stack.pop))
-
-| am_le: 
-  forall (z1 z2: Z) (s:State.t)  (e:Stack.t), 
-     (Stack.push (ZArith.le Stack.pop Stack.pop))
 
 |am_neg_tt: 
   forall  (c:inst) (s:State.t) (e:Stack.t), 
