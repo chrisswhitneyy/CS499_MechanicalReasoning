defca139cc7d025a0b862076d099a558cde8ab68
Christopher Whitney <chrisswhitneyy@gmail.com>
2016-11-17 17:56:08 -0700

    all semantics defined, working on examples.

--- Project2/project2.v
+++ Project2/project2.v
@@ -1,7 +1,8 @@
 (* Project 2: *)
+(* Authors: Chris and Charles Chatwin*)
 
 Require Import Arith ZArith List String State Bool.
-Import ListNotations. 
+Import ListNotations.
 
 (** ** Syntax *)
 (* Translated from page 68 in text, where c is represented using lists*)
@@ -19,111 +20,111 @@ Inductive inst: Type :=
 
 Definition code: Type := list inst.
 
-(* Configuration has the form <c,e,s> where c is a sequence of inst, 
+(* Configuration has the form <c,e,s> where c is a sequence of inst,
     e is the stack, and s is the storage*)
 Definition config : Type := ( code * Stack.t * State.t) .
 
 (** ** Structural Operational Semantics *)
-Inductive am : config -> config -> Prop := 
+Inductive am : config -> config -> Prop :=
+(*NOOP (skip)*)
 | am_noop:
-    forall (c:code) (e:Stack.t)(s:State.t) ,
+    forall (c:code) (e:Stack.t)(s:State.t),
       am  (c, e, s)  (c , e, s)
-| am_push: 
-  forall  (n:Z) (c:code) (e:Stack.t)(s:State.t), 
+(*PUSH*)
+| am_push:
+  forall  (n:Z) (c:code) (e:Stack.t)(s:State.t),
       am ((PUSH n)::c, e, s) (c, (Stack.z n)::e, s)
 
 (* Z operations *)
-| am_add: 
-  forall (z1 z2: Z) (c:code) (s:State.t)  (e:Stack.t), 
+| am_add:
+  forall (z1 z2: Z) (c:code) (s:State.t)  (e:Stack.t),
      am (ADD::c, e, s) (c, (Stack.z (Z.add z1 z2))::e, s)
 
-| am_sub: 
-  forall (z1 z2: Z) (c:code) (s:State.t)  (e:Stack.t), 
+| am_sub:
+  forall (z1 z2: Z) (c:code) (s:State.t)  (e:Stack.t),
      am (SUB::c, e, s) (c, (Stack.z (Z.sub z1 z2))::e, s)
 
-| am_mult: 
-  forall (z1 z2: Z) (c:code) (s:State.t)  (e:Stack.t), 
+| am_mult:
+  forall (z1 z2: Z) (c:code) (s:State.t)  (e:Stack.t),
      am (MULT::c, e, s) (c, (Stack.z (Z.mul z1 z2))::e, s)
 
 (* TRUE and FALSE *)
-| am_true: 
-    forall (c:code) (s:State.t) (e:Stack.t) (tt:bool), 
+| am_true:
+    forall (c:code) (s:State.t) (e:Stack.t) (tt:bool),
        am (TRUE::c, e, s) (c, (Stack.T tt)::e, s)
 
-| am_false: 
-    forall (c:code) (s:State.t) (e:Stack.t) (ff:bool), 
+| am_false:
+    forall (c:code) (s:State.t) (e:Stack.t) (ff:bool),
        am (FALSE::c, e, s) (c, (Stack.T ff)::e, s)
 
-(* EQ and LE *) 
-| am_eq_tt : 
-  forall (c:code) (s:State.t) (e:Stack.t) (z1 z2: Z), 
-    (Z.eqb z1 z2) = true -> 
-    am (EQ::c,e,s) (c, (Stack.T (Z.eqb z1 z2))::e, s)
-| am_eq_ff : 
-  forall (c:code) (s:State.t) (e:Stack.t) (z1 z2: Z), 
-    (Z.eqb z1 z2) = false -> 
+(* EQ and LE *)
+| am_eq_tt :
+  forall (c:code) (s:State.t) (e:Stack.t) (z1 z2: Z),
     am (EQ::c,e,s) (c, (Stack.T (Z.eqb z1 z2))::e, s)
 
-| am_le_tt : 
-  forall (c:code) (s:State.t) (e:Stack.t) (z1 z2: Z), 
-    (Z.leb z1 z2) = true -> 
-    am (LE::c,e,s) (c, (Stack.T (Z.leb z1 z2))::e, s)
-| am_le_ff : 
-  forall (c:code) (s:State.t) (e:Stack.t) (z1 z2: Z), 
-    (Z.leb z1 z2) = false -> 
+| am_le :
+  forall (c:code) (s:State.t) (e:Stack.t) (z1 z2: Z),
     am (LE::c,e,s) (c, (Stack.T (Z.leb z1 z2))::e, s)
 
 (* AND *) 
-| am_and_tt: 
-  forall (c:code) (s:State.t) (e:Stack.t) (t1 t2 tt:bool), 
-    t1 = true -> t2 = true -> 
-    am (AND::c, e, s) (c,(Stack.T tt)::e,s)
+| am_and_tt:
+  forall (c:code) (s:State.t) (e:Stack.t) (t1 t2:bool),
+    am (AND::c, e, s) (c,(Stack.T (andb t1 t2))::e,s)
 
-| am_and_ff: 
-  forall (c:code) (s:State.t) (e:Stack.t) (t1 t2 ff:bool), 
-    t1 = false \/ t2 = false -> 
-    am (AND::c, e, s) (c,(Stack.T ff)::e,s)
- 
 (*NEG *)
-|am_neg_tt: 
-  forall  (c:code) (s:State.t) (e:Stack.t) (t tt:bool), 
-    t = true -> 
-    am (NEG::c,e,s) (c,(Stack.T tt)::e,s)
+|am_neg:
+  forall  (c:code) (s:State.t) (e:Stack.t) (t:bool),
+    am (NEG::c,(Stack.T t)::e,s) (c,(Stack.T (negb t))::e,s)
 
-|am_neg_ff: 
-  forall  (c:code) (s:State.t) (e:Stack.t) (t ff:bool), 
-    t = false -> 
-    am (NEG::c,e,s) (c,(Stack.T ff)::e,s)
-
-| am_fetch: 
-    forall (c:code) (s:State.t) (e:Stack.t) (x:Id.t), 
+(*FETCH*)
+| am_fetch:
+    forall (c:code) (s:State.t) (e:Stack.t) (x:Id.t),
       am ((FETCH x)::c,e,s) (c, (Stack.z (s x))::e,s)
 
+(*STORE*)
 |am_store:
     forall (c:code) (s:State.t) (e:Stack.t) (x:Id.t) (z:Z),
       am ((STORE x)::c,(Stack.z z)::e,s) (c,e, (State.update s x z))
 
-|am_branch_tt: 
-    forall (c c1 c2:code) (s:State.t) (e:Stack.t) (t:bool), 
-      t = true ->
-      am ((BRANCH c1 c2)::c, (Stack.T t)::e, s) (c1++c, e, s)
+(*BRANCH*)
+|am_branch_tt:
+    forall (c c1 c2:code) (s:State.t) (e:Stack.t),
+      am ((BRANCH c1 c2)::c, (Stack.T true)::e, s) (c1++c, e, s)
 
-|am_branch_ff: 
-    forall (c c1 c2:code) (s:State.t) (e:Stack.t) (t:bool), 
-      t = false ->
-      am ((BRANCH c1 c2)::c, (Stack.T t)::e, s) (c2++c, e, s).
+|am_branch_ff:
+    forall (c c1 c2:code) (s:State.t) (e:Stack.t),
+      am ((BRANCH c1 c2)::c, (Stack.T false)::e, s) (c2++c, e, s)
 
-(* LOOP *) 
-(* |am_loop: 
-    forall (c c1 c2:code) (s:State.t) (e:Stack.t), 
-      am ((LOOP (c1++c2)++c), e, s) ( c1::BRANCH(c2::LOOP(c1,c2),NOOP)::c,e,s). *) 
-
-(*
- Module Examples.
-
-    Example ex_4_1: config :=
-      [ (PUSH 1%Z) (Id.Id 0, 3%Z)  [ ] [ ]      ].
+(* LOOP *)
+|am_loop:
+    forall (c c1 c2:code) (s:State.t) (e:Stack.t),
+      am ((LOOP c1 c2)::c, e, s) ( c1++(BRANCH(c2++[(LOOP c1 c2)]) [NOOP])::c,e,s).
 
 
+Module Examples.
 
-  End Examples. *)
+  Definition x : Id.t := Id.Id 0.
+
+  Definition s (y: Id.t) : Z := 
+    if Id.beq y x 
+      then 3%Z
+    else 0%Z.
+
+  Definition s' (y: Id.t) : Z := 
+    if Id.beq y x 
+      then 4%Z
+    else 0%Z.
+
+   Example ex_4_1: 
+      am ([ (PUSH 1%Z) ; (FETCH x) ; (ADD) ; (STORE x) ],[ ], s )  ([],[],s') . 
+   Proof. 
+       admit.
+   Admitted.
+
+   Example ex_4_2: 
+      am ( [ LOOP [TRUE] [NOOP] ], [ ], s) ([ ], [ ], s). 
+  Proof. 
+    admit. 
+  Admitted.
+
+End Examples. 
